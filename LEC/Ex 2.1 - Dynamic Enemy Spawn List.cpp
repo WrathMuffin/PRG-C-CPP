4c4134daab89fd24cd708662ae794e641b89f9b7
Chuc <65578837+WrathMuffin@users.noreply.github.com>
2026-03-19 11:02:07 -0400

    uhh
    uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ LEC/Ex 2.1 - Dynamic Enemy Spawn List.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+    int count;
+    cout << "How many enemies do you want to spawn?";
+    cin >> count;
+
+    string *enemyType = new string[count];
+
+
+
+    for (int i = 0; i < count; i ++)
+    {
+        enemyType[i];
+    }
+
+    return 0;
+}
